fabdfb60739679497cad1316064cf30b8409a495
Attempt4 <t1029@protonmail.com>
2025-02-10 23:35:52 +0000

    Seperated Lambert solver to seperate function in encounter.py. Defined parameters in dictionary P_CONFIG which is passed into the function, defining Spice kernel parameters. Clean up required

---
+++ Gravitas_Py_3.0/Porkchop_3.0/encounter.py
@@ -0,0 +1,123 @@
+#ephemeris_plot.py
+
+"""""""""""""""""""""""""""""""""
+----------------------------------
+GRAVITAS V.3.0 - Porkchop Plotter
+by Tomas Bezkorowajnyj c. February 2025
+----------------------------------
+"""""""""""""""""""""""""""""""""
+
+
+import numpy as np
+
+from video_manager import *
+
+from N_Body_Physics.Trajectory_Char import *
+
+from N_Body_Physics.Physics import *
+
+from display_manager import *
+
+import pathlib
+
+import spiceypy as spice
+
+from poliastro.iod import izzo
+
+from poliastro.bodies import Sun, Earth
+
+from astropy import units as u
+
+
+class P_CONFIG:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)  # Assign all key-value pairs as attributes
+
+def P_Solve(pd, sp_path):
+
+    config = P_CONFIG(**pd)
+
+    ##  Set the path to the image folder
+    image_folder = f"{sp_path}/fig/"
+
+    ##  Set the desired output video file name
+    output_video_file = f"{sp_path}/video_output/output_video.mp4"
+
+    ##  Output file name
+    output_plot_title = config.out_title
+
+
+    ##  Debug Package Loading
+    print(spice.tkvrsn('TOOLKIT'))
+
+
+
+    ##--Define departure and arrival parameters from spice kernels--##
+
+    #   Load SPICE kernels
+    spice.furnsh(f"{sp_path}/Spice_Kernels/de421.bsp")  # Ephemeris data
+    spice.furnsh(f"{sp_path}/Spice_Kernels/naif0012.tls")  # leap seconds
+
+
+    #   Convert to Ephemeris Time
+    et_de0 = spice.str2et(config.d_time0)
+    et_de1 = spice.str2et(config.d_time1)
+
+    et_ar0 = spice.str2et(config.a_time0)
+    et_ar1 = spice.str2et(config.a_time1)
+
+
+    ##  Ephemeris time arrays
+    ets_de = np.arange(et_de0, et_de1, config.step)
+    ets_ar = np.arange(et_ar0, et_ar1, config.step)
+
+
+    #   Collect trajectory data
+    trajectory_departure = np.array([spice.spkezr(config.origin, et, config.frame, config.abcorr, config.observer)[0] for et in ets_de])
+    trajectory_arrival = np.array([spice.spkezr(config.target, et, config.frame, config.abcorr, config.observer)[0] for et in ets_ar])
+
+
+    ##  Define array packets
+    c3_meshgrid = np.zeros((len(trajectory_arrival),len(trajectory_departure)))
+    vinf_meshgrid = np.zeros((len(trajectory_arrival),len(trajectory_departure)))
+    #vel_meshgrid = np.zeros((len(trajectory_arrival),len(trajectory_departure), 3))
+    tof_meshgrid = np.zeros((len(trajectory_arrival),len(trajectory_departure)))
+
+
+
+    ##--Programming Loop--##
+
+    for i in range(len(trajectory_arrival)):
+
+
+        print(f'{i} of {len(trajectory_arrival)}')
+
+        for j in range(len(trajectory_departure)):
+            
+            ##  Define velocity vectors
+            v0 = 0
+            v = 0
+            vp = trajectory_departure[j][3:]
+            va = trajectory_arrival[i][3:]
+
+
+            ##  Izzo Lambert Solver
+            lambert = izzo.lambert(Sun.k, trajectory_departure[j][:3]*u.km, trajectory_arrival[i][:3]*u.km, (ets_ar[i]*u.s - ets_de[j]*u.s),M=0)   # :3
+
+
+            ##  Unpack initial and final velocity
+            v0, v = next(lambert)
+
+
+            ##  Solve characteristic velocity and v-infinity
+            c3 = np.linalg.norm(v0.value - vp)**2
+            v_inf_ar = np.linalg.norm(v.value - va)
+
+
+            ##  Store values in array packets
+            c3_meshgrid[i][j] = c3
+            vinf_meshgrid[i][j] = v_inf_ar
+            tof_meshgrid[i][j] = (ets_ar[i] - ets_de[j])/(3600*24)
+            #vel_vec[i][j] = v0.value
+    
+    return np.array([c3_meshgrid, vinf_meshgrid, tof_meshgrid])
--- Gravitas_Py_3.0/Porkchop_3.0/ephemeris_plot.py
+++ Gravitas_Py_3.0/Porkchop_3.0/ephemeris_plot.py
@@ -7,141 +7,36 @@ by Tomas Bezkorowajnyj c. February 2025
 ----------------------------------
 """""""""""""""""""""""""""""""""
 
-
-import numpy as np
-
 from matplotlib import pyplot as plt
 
-from video_manager import *
+from encounter import *
 
-from N_Body_Physics.Trajectory_Char import *
+# Instantiating with a dictionary-like structure
+m_2020 = {
+    "d_time0":      "2020-05-01T00:00:00",
+    "d_time1":      "2020-11-01T00:00:00",
+    "a_time0":      "2020-12-01T00:00:00",
+    "a_time1":       "2022-01-01T00:00:00",
 
-from N_Body_Physics.Physics import *
+    "target":       "MARS BARYCENTER",
+    "origin":       "EARTH BARYCENTER",
+    "observer":     "SOLAR SYSTEM BARYCENTER",
+    "frame":        "ECLIPJ2000",
+    "abcorr":       "NONE",
 
-from display_manager import *
-
-import pathlib
-
-import spiceypy as spice
-
-from poliastro.iod import izzo
-
-from poliastro.bodies import Sun, Earth
-
-from astropy import units as u
-
+    "step":         3600*12*5,
+    "out_title":    "Mars 2020 Transfer Window",
+}
 
 
 ##  Retrieve local file directory
 dir = pathlib.Path(__file__).parent.resolve()
 
-##  Set the path to the image folder
-image_folder = f"{dir}/fig/"
-
-##  Set the desired output video file name
-output_video_file = f"{dir}/video_output/output_video.mp4"
-
-##  Output file name
-output_plot_title = "Mars 2020 Transfer Window"
-
-
-##  Debug Package Loading
-print(dir)
-print(spice.tkvrsn('TOOLKIT'))
-
-
-
-##--Define departure and arrival parameters from spice kernels--##
-
-#   Load SPICE kernels
-spice.furnsh(f"{dir}/Spice_Kernels/de421.bsp")  # Ephemeris data
-spice.furnsh(f"{dir}/Spice_Kernels/naif0012.tls")  # leap seconds
-
-
-#   Time step
-step = 3600*12*5  # seconds
-
-
-#   Define departure times
-departure_time0 = "2020-05-01T00:00:00"
-departure_time1 = "2020-11-01T00:00:00"
-
-#   Define arrival times
-arrival_time0 = "2020-12-01T00:00:00"
-arrival_time1 = "2022-01-01T00:00:00"
-
-
-#   Convert to Ephemeris Time
-et_de0 = spice.str2et(departure_time0)
-et_de1 = spice.str2et(departure_time1)
-
-et_ar0 = spice.str2et(arrival_time0)
-et_ar1 = spice.str2et(arrival_time1)
-
-
-##  Ephemeris time arrays
-ets_de = np.arange(et_de0, et_de1, step)
-ets_ar = np.arange(et_ar0, et_ar1, step)
-
-
-#   Define origin, target, and observer
-target = "MARS BARYCENTER"
-origin = "EARTH BARYCENTER"
-observer = "SOLAR SYSTEM BARYCENTER"
-frame = "ECLIPJ2000"
-abcorr = "NONE"
-
-
-#   Collect trajectory data
-trajectory_departure = np.array([spice.spkezr(origin, et, frame, abcorr, observer)[0] for et in ets_de])
-trajectory_arrival = np.array([spice.spkezr(target, et, frame, abcorr, observer)[0] for et in ets_ar])
-
-
-##  Define array packets
-porkchop_array = np.zeros((len(trajectory_arrival),len(trajectory_departure)))
-vinfinity_array = np.zeros((len(trajectory_arrival),len(trajectory_departure)))
-vel_vec = np.zeros((len(trajectory_arrival),len(trajectory_departure), 3))
-time_of_flight = np.zeros((len(trajectory_arrival),len(trajectory_departure)))
-
-
-
-##--Programming Loop--##
-
-for i in range(len(trajectory_arrival)):
-
-
-    print(f'{i} of {len(trajectory_arrival)}')
-
-    for j in range(len(trajectory_departure)):
-        
-        ##  Define velocity vectors
-        v0 = 0
-        v = 0
-        vp = trajectory_departure[j][3:]
-        va = trajectory_arrival[i][3:]
-
-
-        ##  Izzo Lambert Solver
-        lambert = izzo.lambert(Sun.k, trajectory_departure[j][:3]*u.km, trajectory_arrival[i][:3]*u.km, (ets_ar[i]*u.s - ets_de[j]*u.s),M=0)   # :3
-
-
-        ##  Unpack initial and final velocity
-        v0, v = next(lambert)
-
-
-        ##  Solve characteristic velocity and v-infinity
-        c3 = np.linalg.norm(v0.value - vp)**2
-        v_inf_ar = np.linalg.norm(v.value - va)
-
-
-        ##  Store values in array packets
-        porkchop_array[i][j] = c3
-        vinfinity_array[i][j] = v_inf_ar
-        time_of_flight[i][j] = (ets_ar[i] - ets_de[j])/(3600*24)
-        vel_vec[i][j] = v0.value
-
 
 ##--Plot porkchops with c3 and v-infinity data--##
+
+porkchop_array, vinfinity_array, time_of_flight = P_Solve(m_2020, dir)
+
 
 ##  Define figure
 fig, ax = plt.subplots(figsize=(10,12))
@@ -155,11 +50,11 @@ ax.clabel(CS1, inline=1, fontsize=10)
 CS2 = ax.contour(time_of_flight, colors=[(0.0,1.0,0.0)])
 ax.clabel(CS2, inline=1, fontsize=10)
 
-plt.title(output_plot_title)
-plt.xlabel(f'Departure Window (Days after UST:{departure_time0})')
-plt.ylabel(f'Arrival Window (Days after UST:{arrival_time0})')
+plt.title(m_2020["out_title"])
+plt.xlabel(f'Departure Window (Days after UST:{m_2020["d_time0"]})')
+plt.ylabel(f'Arrival Window (Days after UST:{m_2020["a_time1"]})')
 
-plt.savefig(f'{dir}/video_output/{output_plot_title}.png')
+plt.savefig(f'{dir}/video_output/{m_2020["out_title"]}.png')
 
 plt.close()
 
@@ -177,7 +72,7 @@ print('v-inf min: ', v_inf_min)
 
 min_i = 0
 min_j = 0
-
+"""
 for i in range(len(trajectory_arrival)):
     for j in range(len(trajectory_departure)):
         if porkchop_array[i][j] == c3_min:
@@ -206,3 +101,4 @@ ax.plot(aw2[0],-aw2[1],-aw2[2],label="orbit")
 
 plt.savefig(f'{dir}/video_output/min_traj.png',dpi=300)
 plt.show()
+"""
