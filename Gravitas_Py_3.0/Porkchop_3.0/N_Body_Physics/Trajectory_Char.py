Fixed yet another unit conversion error so now the planetary configuration can be seen at the point of minimum c3 within the window.
Still issues with projection function plotTraj() that are unresolved.

Gravitas_Py_3.0/Porkchop_3.0/N_Body_Physics/Trajectory_Char.py
@@ -12,15 +12,15 @@ def plotOrbs(i, sp_obj):
     V_e = auday*np.array([sp_obj[3][i],sp_obj[4][i],sp_obj[5][i]])
 
     H_e = np.cross(R_e, V_e)
-    R_e_mod = np.norm(R_e)
-    V_e_mod = np.norm(V_e)
+    R_e_mod = np.linalg.norm(R_e)
+    V_e_mod = np.linalg.norm(V_e)
     E_e = V_e_mod**2 / 2 - mu / R_e_mod
-    H_e_mod = np.norm(H_e)
+    H_e_mod = np.linalg.norm(H_e)
     e_e = np.cross(V_e, H_e) / mu - R_e / R_e_mod
     n_e = np.cross([0,0,1], H_e)
     i_e = np.arccos(H_e[2]/H_e_mod)
-    W_e = np.arccos(n_e[0]/np.norm(n_e))
-    w_e = np.arccos(np.dot(n_e,e_e)/(np.norm(n_e)*np.norm(e_e)))
+    W_e = np.arccos(n_e[0]/np.linalg.norm(n_e))
+    w_e = np.arccos(np.dot(n_e,e_e)/(np.linalg.norm(n_e)*np.linalg.norm(e_e)))
     a_e = -mu/(2*E_e)
 
     return displayOrbit(a_e,e_e,i_e,w_e,W_e)
@@ -31,15 +31,15 @@ def getPlanetOrbit(i, sp_obj):
     V_e = auday*np.array([sp_obj[3][i],sp_obj[4][i],sp_obj[5][i]])
 
     H_e = np.cross(R_e, V_e)
-    R_e_mod = np.norm(R_e)
-    V_e_mod = np.norm(V_e)
+    R_e_mod = np.linalg.norm(R_e)
+    V_e_mod = np.linalg.norm(V_e)
     E_e = V_e_mod**2 / 2 - mu / R_e_mod
-    H_e_mod = np.norm(H_e)
+    H_e_mod = np.linalg.norm(H_e)
     e_e = np.cross(V_e, H_e) / mu - R_e / R_e_mod
     n_e = np.cross([0,0,1], H_e)
     i_e = np.arccos(H_e[2]/H_e_mod)
-    W_e = np.arccos(n_e[0]/np.norm(n_e))
-    w_e = np.arccos(np.dot(n_e,e_e)/(np.norm(n_e)*np.norm(e_e)))
+    W_e = np.arccos(n_e[0]/np.linalg.norm(n_e))
+    w_e = np.arccos(np.dot(n_e,e_e)/(np.linalg.norm(n_e)*np.nlinalg.normorm(e_e)))
     a_e = -mu/(2*E_e)
 
     return np.array([a_e,e_e,i_e,w_e,W_e])
@@ -50,15 +50,15 @@ def getPlanetOrbit(i, sp_obj):
 def plotTraj(V,R):
 
     H_e = np.cross(R, V)
-    R_mod = np.norm(R)
-    V_mod = np.norm(V)
+    R_mod = np.linalg.norm(R)
+    V_mod = np.linalg.norm(V)
     E_e = V_mod**2 / 2 - mu / R_mod
-    H_e_mod = np.norm(H_e)
+    H_e_mod = np.linalg.norm(H_e)
     e_e = np.cross(V, H_e) / mu - R / R_mod
     n_e = np.cross([0,0,1], H_e)
     i_e = np.arccos(H_e[2]/H_e_mod)
-    W_e = np.arccos(n_e[0]/np.norm(n_e))
-    w_e = np.arccos(np.dot(n_e,e_e)/(np.norm(n_e)*np.norm(e_e)))
+    W_e = np.arccos(n_e[0]/np.linalg.norm(n_e))
+    w_e = np.arccos(np.dot(n_e,e_e)/(np.linalg.norm(n_e)*np.linalg.norm(e_e)))
     a_e = -mu/(2*E_e)
 
     return displayOrbit(a_e,e_e,i_e,w_e,W_e)

Gravitas_Py_3.0/Porkchop_3.0/display_manager.py
@@ -9,7 +9,7 @@ def displayOrbit(a,e,i,omega,Omega):
     nu = np.linspace(0, 2 * np.pi, 500)
 
     # Calculate the radius for each true anomaly (orbit equation)
-    r = (a * (1 - np.norm(e)**2)) / (1 + np.norm(e) * np.cos(nu))
+    r = (a * (1 - np.linalg.norm(e)**2)) / (1 + np.linalg.norm(e) * np.cos(nu))
 
     # Position in the orbital plane (x', y')
     x_prime = r * np.cos(nu)