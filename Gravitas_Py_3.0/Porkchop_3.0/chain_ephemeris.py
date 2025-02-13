148087110b5cfe11dd56002d7c1d76eb506a0048
Attempt4 <t1029@protonmail.com>
2025-02-13 22:59:43 +0000

    Implementation of v-infinity matching reworked in a correct manner. Development still steadily moving. Plottable graphs of delta_v-infinity in chain_ephemeris.py

--- Gravitas_Py_3.0/Porkchop_3.0/chain_ephemeris.py
+++ Gravitas_Py_3.0/Porkchop_3.0/chain_ephemeris.py
@@ -13,6 +13,8 @@ from encounter import *
 
 import scipy.interpolate as sc
 
+from datetime import datetime, timedelta
+
 
 # Instantiating with a dictionary-like structure
 m_2020_1 = {
@@ -47,6 +49,23 @@ m_2020_2 = {
     "out_title":    "Mars 2020",
 }
 
+match_vm = {
+    "d_time0":      "2021-04-01T00:00:00",
+    "a_time0":      "2021-01-01T00:00:00",
+    "a_time0":      "2021-01-01T00:00:00",
+    "a_time1":      "2023-01-01T00:00:00",
+
+    "target":       "MARS BARYCENTER",
+    "origin":       "VENUS BARYCENTER",
+    "observer":     "SOLAR SYSTEM BARYCENTER",
+    "frame":        "ECLIPJ2000",
+    "abcorr":       "NONE",
+
+    "step":         3600*12*5,
+    "out_title":    "Mars 2020",
+}
+
+
 
 ##  Retrieve local file directory
 dir = pathlib.Path(__file__).parent.resolve()
@@ -56,26 +75,39 @@ dir = pathlib.Path(__file__).parent.resolve()
 
 v0,v1,vp,va  = P_Solve(m_2020_1, dir)
 
-vinf = v1 - va
+c3 = np.linalg.norm(v0 - vp, axis=2)**2
+vi = np.linalg.norm(v1 - va, axis=2)
 
-meshgrid_matrix = np.zeros((vinf.shape[0],vinf.shape[1]))
-
-
-for trial_index in range(vinf.shape[1]):
-
-    tof_pm, dv_pm  = P_Match(m_2020_2, dir, trial_index, vinf[:,trial_index,:])
-
-    meshgrid_matrix[:,trial_index] = dv_pm
-
-##  Define figure
-fig, ax = plt.subplots(figsize=(10,12))
+c3_min = np.min(c3)
+vi_min = np.min(vi)
 
 
+min_ind_c = np.unravel_index(np.argmin(c3), c3.shape)
+min_ind_v = np.unravel_index(np.argmin(vi), vi.shape)
 
-CS = ax.contour(meshgrid_matrix, levels=np.linspace(0,0.1,1), colors=[(0.0,0.0,1.0)])
-ax.clabel(CS, inline=1, fontsize=10)
+v0_vec = (v0 - vp)
+vi_vec = v1 - va
+
+c3_min_v = v0_vec[min_ind_c]
+vp_min_v = vp[min_ind_c]
+vi_min_c = vi_vec[min_ind_v]
+va_min_v = va[min_ind_v]
+
+
+print(np.linalg.norm(c3_min_v)**2, ' ', np.linalg.norm(vi_min_c))
+print(c3_min, ' ', vi_min)
+
+tppp, vppp = P_Match(match_vm, dir, vi_min_c, 3600*24*200)
+
+plt.plot(tppp, vppp)
 
 plt.show()
+
+
+#CS = ax.contour(meshgrid_matrix, levels=np.linspace(0,0.1,1), colors=[(0.0,0.0,1.0)])
+#ax.clabel(CS, inline=1, fontsize=10)
+
+#plt.show()
 
 ##  Define meshgrids for contour plot
 #c3 = np.linalg.norm(v0 - vp, axis=2)**2
--- Gravitas_Py_3.0/Porkchop_3.0/encounter.py
+++ Gravitas_Py_3.0/Porkchop_3.0/encounter.py
@@ -28,6 +28,22 @@ from poliastro.bodies import Sun, Earth
 
 from astropy import units as u
 
+from datetime import datetime, timedelta
+
+
+def add_time_to_datetime(time_str, delt):   ##  Add time in seconds
+
+    # Convert to datetime object
+    time_obj = datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%S")
+
+    # Add 60 days
+    new_time_obj = time_obj + timedelta(seconds = delt)
+
+    # Convert back to string in the same format
+    new_time_str = new_time_obj.strftime("%Y-%m-%dT%H:%M:%S")
+
+    return new_time_str
+
 
 class P_CONFIG:
     def __init__(self, **kwargs):
@@ -99,16 +115,22 @@ def P_Solve(pd, sp_path):
 
         for j in range(len(trajectory_departure)):
             
+            ##  Define velocity vectors
+            v0 = 1e20*u.km/u.s
+            v = 1e20*u.km/u.s
+            vp = trajectory_departure[j][3:]
+            va = trajectory_arrival[i][3:]
+
+
             if ets_ar[i] - ets_de[j] < 0:
                 tf_m[i][j] = 1e20
+                v0_m[i][j] = 1e20
+                v1_m[i][j] = 1e20
+                vp_m[i][j] = vp
+                va_m[i][j] = va
                 continue
 
 
-            ##  Define velocity vectors
-            v0 = 0*u.km/u.s
-            v = 0*u.km/u.s
-            vp = trajectory_departure[j][3:]
-            va = trajectory_arrival[i][3:]
 
 
             ##  Izzo Lambert Solver
@@ -143,7 +165,7 @@ def P_Solve(pd, sp_path):
 
 
 
-def P_Match(pd, sp_path, j_ind, v_infinity):
+def P_Match(pd, sp_path, v_infinity, time_of_flight):
 
     config = P_CONFIG(**pd)
 
@@ -168,67 +190,56 @@ def P_Match(pd, sp_path, j_ind, v_infinity):
     spice.furnsh(f"{sp_path}/Spice_Kernels/de421.bsp")  # Ephemeris data
     spice.furnsh(f"{sp_path}/Spice_Kernels/naif0012.tls")  # leap seconds
 
+    second_time = add_time_to_datetime(config.d_time0, time_of_flight)
 
     #   Convert to Ephemeris Time
     et_de0 = spice.str2et(config.d_time0)
-    et_de1 = spice.str2et(config.d_time1)
 
     et_ar0 = spice.str2et(config.a_time0)
-    et_ar1 = spice.str2et(config.a_time1)
+    et_ar1 = spice.str2et(second_time)
 
 
     ##  Ephemeris time arrays
-    ets_de = np.arange(et_de0, et_de1, config.step)
     ets_ar = np.arange(et_ar0, et_ar1, config.step)
 
 
     #   Collect trajectory data
-    trajectory_departure = np.array([spice.spkezr(config.origin, et, config.frame, config.abcorr, config.observer)[0] for et in ets_de])
+    trajectory_departure = spice.spkezr(config.origin, et_de0, config.frame, config.abcorr, config.observer)[0]
+
     trajectory_arrival = np.array([spice.spkezr(config.target, et, config.frame, config.abcorr, config.observer)[0] for et in ets_ar])
 
     tf_m = np.zeros((len(trajectory_arrival)))
     dv_inf = np.zeros((len(trajectory_arrival), 3))
 
+
     ##--Programming Loop--##
-
-    debug_message = ''
-
-    CURSOR_UP = "\033[1A"
-    CLEAR = "\x1b[2K"
 
     print('Getting Matching array...')
 
-    for i in range(len(trajectory_arrival)):
-
-        if ets_ar[i] - ets_de[j_ind] < 0:
-            tf_m[i] = 1e20
-            continue
-
+    for i in range(len(ets_ar)):
 
         ##  Define velocity vectors
         v0 = 0*u.km/u.s
         v = 0*u.km/u.s
-        vp = trajectory_departure[j_ind][3:]
+        vp = trajectory_departure[3:]
         va = trajectory_arrival[i][3:]
 
 
         ##  Izzo Lambert Solver
         try:
-            lambert = izzo.lambert(Sun.k, trajectory_departure[j_ind][:3]*u.km, trajectory_arrival[i][:3]*u.km, (ets_ar[i]*u.s - ets_de[j_ind]*u.s),M=0)   # :3
+            lambert = izzo.lambert(Sun.k, trajectory_departure[:3]*u.km, trajectory_arrival[i][:3]*u.km, ets_ar[i]*u.s,M=0)   # :3
             v0, v = next(lambert)
 
-            tf_m[i] = (ets_ar[i] - ets_de[j_ind])/(3600*24)
+            tf_m[i] = ets_ar[i]/(3600*24)
         except:
             v0, v = 1e10*u.km/u.s, 1e10*u.km/u.s
 
 
-        dv_inf[i] = (v0.value - vp)
-
-        print(CURSOR_UP + CLEAR, end="")
+        dv_inf[i,:] = (v0.value - vp)
 
     v_i = np.linalg.norm(v_infinity - dv_inf, axis=1)
 
-    return np.array([tf_m, v_i], dtype=object)
+    return np.array([ets_ar, v_i], dtype=object)
 
 
 
