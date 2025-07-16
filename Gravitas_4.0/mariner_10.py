d755e90b9261864ffce9af0142802288213d747d
Attempt4 <t1029@protonmail.com>
2025-07-16 17:50:20 +0100

    failsafe installed into mariner_10.py to mitigate data leaks in case of early interrupt or error.
    There are a few limitations to the code which I don't quite understand yet but it has something to do with the indexing.

--- Gravitas_4.0/mariner_10.py
+++ Gravitas_4.0/mariner_10.py
@@ -18,64 +18,72 @@ from spice_video import visual_animation_3
 PROJ_DIR = pathlib.Path(__file__).parent.resolve()
 MU = 6.67e-20*1.989e30  ## in km
 EPS = 0.01
+CLOSEST_APPROACH = 1.182e4
+
+T_INDEX = 60
 
 ## Transfer between planets as layed out in transfer definition
-setup_kernel()
 
-ev_v0, ev_v1, ev_vp, ev_va  = generate_porkchop(MARINER_10_EV)
-vm_v0, vm_v1, vm_vp, vm_va  = generate_porkchop(MARINER_10_VM)
-ev_c3 = np.linalg.norm(ev_v0 - ev_vp, axis=2)**2
-ev_vi = np.linalg.norm(ev_v1 - ev_va, axis=2)
-vm_c3 = np.linalg.norm(vm_v0 - vm_vp, axis=2)**2
-vm_vi = np.linalg.norm(vm_v1 - vm_va, axis=2)
+def mariner_10():
 
-vinf = []
-for _t in range(45):
-    vinf.append(vinfinity_match_3(MARINER_10_EV, MARINER_10_VM, ev_v1-ev_va, vm_v0-vm_vp, _t))
-vinf = np.array(vinf)
-v_inf_in = ev_v1-ev_va
-v_inf_ou = vm_v0-vm_vp  #).transpose(1,0,2)
+    ev_v0, ev_v1, ev_vp, ev_va  = generate_porkchop(MARINER_10_EV)
+    vm_v0, vm_v1, vm_vp, vm_va  = generate_porkchop(MARINER_10_VM)
+    ev_c3 = np.linalg.norm(ev_v0 - ev_vp, axis=2)**2
+    ev_vi = np.linalg.norm(ev_v1 - ev_va, axis=2)
+    vm_c3 = np.linalg.norm(vm_v0 - vm_vp, axis=2)**2
+    vm_vi = np.linalg.norm(vm_v1 - vm_va, axis=2)
+    v_inf_in = ev_v1-ev_va
+    v_inf_ou = vm_v0-vm_vp
 
-i_gh = np.min([len(v_inf_in), len(v_inf_ou)])
-j_gh = np.min([len(v_inf_in[0]), len(v_inf_ou[0])])
-
-print(v_inf_in.shape)
-print(v_inf_ou.shape)
-print(i_gh)
-print(j_gh)
-
-v_x_t = []
-v_y_t = []
-
-v_match = []
-
-for _t in range(45):
-    v_x = []
-    v_y = []
-
-    for i in range(i_gh):
-        for j in range(j_gh):
-            ab_dot = np.dot(v_inf_in[_t][j],v_inf_ou[i][_t])
-            ab_prod = np.linalg.norm(v_inf_in[_t][j]) * np.linalg.norm(v_inf_ou[i][_t])
-            theta = 0.5 * np.arccos( ab_dot / ab_prod )
-            r_pfb = (MU / np.linalg.norm(v_inf_ou[i][_t])**2) * (-1 + 1 / np.sin(theta)) * 2E-6
-            vi = np.sqrt(ev_c3[_t][j])
-            
-            if np.abs(vinf[_t][j][i]) < EPS and 2500. < r_pfb < 20000. and vi < 4.5:
-                v_match.append([j, i, _t, r_pfb, vi])
-
-v_match = np.array(v_match).T
-
-print(v_match)
-
-departure_time = datetime.strptime(MARINER_10_EV['d_time0'], "%Y-%m-%dT%H:%M:%S") + timedelta(seconds=float(35)*MARINER_10_EV['step'])
-arrival_time = datetime.strptime(MARINER_10_EV['a_time0'], "%Y-%m-%dT%H:%M:%S") + timedelta(seconds=float(35)*MARINER_10_EV['step'])
+    vinf = []
+    for _t, _ in enumerate(v_inf_in):
+        vinf.append(vinfinity_match_3(MARINER_10_EV, MARINER_10_VM, ev_v1-ev_va, vm_v0-vm_vp, _t))
+    vinf = np.array(vinf)
 
 
-#visual_animation_3(MARINER_10_EV,MARINER_10_VM, departure_time, arrival_time, ev_v0[35][33], 35)
+    i_gh = np.min([len(v_inf_in), len(v_inf_ou)])
+    j_gh = np.min([len(v_inf_in[0]), len(v_inf_ou[0])])
 
+    print(v_inf_in.shape)
+    print(v_inf_ou.shape)
+    print(i_gh)
+    print(j_gh)
 
+    v_match = []
 
-plt.show()
+    for _t, _ in enumerate(v_inf_in):
+        for i in range(i_gh):
+            for j in range(j_gh):
+                ab_dot = np.dot(v_inf_in[_t][j],v_inf_ou[i][_t])
+                ab_prod = np.linalg.norm(v_inf_in[_t][j]) * np.linalg.norm(v_inf_ou[i][_t])
+                theta = 0.5 * np.arccos( ab_dot / ab_prod )
+                r_pfb = (MU / np.linalg.norm(v_inf_ou[i][_t])**2) * (-1 + 1 / np.sin(theta)) * 2E-6
+                vi = np.sqrt(ev_c3[_t][j])
 
-destroy_kernel()
+                if np.abs(vinf[_t][i][j]) < EPS and 6052. < r_pfb < 50000.:
+                    v_match.append([j, i, _t, r_pfb, vi])
+
+    v_match = np.array(v_match)
+
+    v_inf_match = []
+
+    for r in v_match:
+        e_imp = datetime.strptime(MARINER_10_EV['d_time0'], "%Y-%m-%dT%H:%M:%S") + timedelta(seconds=float(r[0])*MARINER_10_EV['step'])
+        v_imp = datetime.strptime(MARINER_10_EV['a_time0'], "%Y-%m-%dT%H:%M:%S") + timedelta(seconds=float(r[2])*MARINER_10_EV['step'])
+        m_imp = datetime.strptime(MARINER_10_VM['a_time0'], "%Y-%m-%dT%H:%M:%S") + timedelta(seconds=float(r[1])*MARINER_10_EV['step'])
+        v_inf_match.append([str(e_imp), str(v_imp), str(m_imp), int(r[3]), float(r[4])])
+
+    for r in v_inf_match:
+        print(r)
+    print(len(v_inf_match))
+
+if __name__ == "__main__":
+
+    setup_kernel()
+
+    try:
+        mariner_10()
+    except Exception as e:
+        print(f'error: \n{e}')
+    finally:
+        destroy_kernel()
